be9bab916a87be0423c2986dc6ba4f8bcb7acac6
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2025-10-29 23:19:48 +0100

    update

--- src/main.c
+++ src/main.c
@@ -831,7 +831,7 @@ static void ping_timer_handler(__attribute__ ((unused)) void *arg)
     {
         struct mg_mqtt_opts opts = {
             .topic = mg_str("nyx/ping/node"),
-            .message = mg_str("Nyx Stream"),
+            .message = mg_str("Nyx Stream Server"),
         };
 
         mg_mqtt_pub(mqtt_conn, &opts);
